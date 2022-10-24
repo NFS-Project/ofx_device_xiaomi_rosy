3203ea15abcdd0d293bf60a8a0bedcc1ee42f5e7
AnGgIt88 <jarbull87@gmail.com>
2022-10-24 17:25:15 +0000

    rosy: Bringup for fox_12.1
    Regenrate from https://github.com/TeamWin/android_device_xiaomi_mi8937.git
    
    and https://gitlab.com/OrangeFox/device/mido.git
    
    Signed-off-by: AnGgIt88 <jarbull87@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,109 +1,29 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2022 Team Win Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2022 The OrangeFox Recovery Project
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+# use our own fstab+flags
+MITHORIUM_USES_DEVICE_SPECIFIC_FSTAB := true
+
+# Inherit from common mithorium-common
+include device/xiaomi/mithorium-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/rosy
 
-# Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8953
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
-TARGET_BOARD_SUFFIX := _64
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8953
-TARGET_NO_BOOTLOADER := true
-
 # Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := \
-	androidboot.hardware=qcom \
-	msm_rtb.filter=0x237 \
-	ehci-hcd.park=3 \
-	lpm_levels.sleep_disabled=1 \
-	androidboot.bootdevice=7824900.sdhci \
-	earlycon=msm_hsl_uart,0x78B0000 \
-	androidboot.selinux=permissive
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 
-# Encryption
-TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
-TARGET_HW_DISK_ENCRYPTION := true
+# no EDL mode
+TW_HAS_EDL_MODE := false
 
-# Keymaster
-TARGET_PROVIDES_KEYMASTER := true
-
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 58846064640 # (58846081024-16384)
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_VENDORIMAGE_PARTITION_SIZE := 536870912
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-
-# Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Qualcomm support
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-
-# TWRP Configuration
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_EXTRA_LANGUAGES := true
-TW_THEME := portrait_hdpi
-TW_INCLUDE_CRYPTO := true
+# brightness
 TW_MAX_BRIGHTNESS := 255
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_SCREEN_BLANK_ON_BOOT := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_SDCARD_ON_DATA := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_USE_TOOLBOX := true
+TW_DEFAULT_BRIGHTNESS := 145
 
-# Disable Mouse Cursor
-TW_INPUT_BLACKLIST := "hbtp_vm"
-
-# exFAT FS Support
-TW_INCLUDE_FUSE_EXFAT := true
-
-# NTFS Support
-TW_INCLUDE_FUSE_NTFS := true
+# Recovery
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
+#
